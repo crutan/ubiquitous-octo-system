b7c9645ae93ad35fdf69201f9391ddb7fc31c60a
Caleb Rutan <crutan@gmail.com>
2022-09-21 04:10:22 +0000

    working fade in/out transitions

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,18 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import "trix"
 import "@rails/actiontext"
+
+
+document.addEventListener("turbo:before-stream-render", (event) => {
+  if (event.target.action === 'replace') {
+    const targetFrame = document.getElementById(event.target.target)
+    if (targetFrame.dataset.animateOut) {
+      event.preventDefault()
+      const elementBeingAnimated = targetFrame
+      elementBeingAnimated.classList.add(targetFrame.dataset.animateOut)
+      elementBeingAnimated.addEventListener("animationend", () => {
+        event.target.performAction()
+      })
+    }
+  }
+})
