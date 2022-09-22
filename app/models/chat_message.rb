d0d56cf73b547b36e910b9a1547e5332991a3a8a
Caleb Rutan <crutan@gmail.com>
2022-09-22 04:00:52 +0000

    add time zone config, move chat broadcast to later

--- app/models/chat_message.rb
+++ app/models/chat_message.rb
@@ -4,6 +4,6 @@ class ChatMessage < ApplicationRecord
   belongs_to :attendee
   belongs_to :presentation
 
-  after_create_commit ->(message) { broadcast_append_to(message.presentation, 'chat_messages') }
-  after_destroy_commit ->(message) { broadcast_remove_to(message.presentation, 'chat_messages') }
+  after_create_commit ->(message) { broadcast_append_later_to(message.presentation, 'chat_messages') }
+  after_destroy_commit ->(message) { broadcast_remove_later_to(message.presentation, 'chat_messages') }
 end
